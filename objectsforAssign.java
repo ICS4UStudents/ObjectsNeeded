0c64861c46406933e62d69e498f567dc58cd07ef
Botanahmad <botanahmad12345@gmail.com>
2017-05-17 13:02:11 -0400

    Add files via upload

---
+++ objectsforAssign.java
@@ -0,0 +1,21 @@
+class objectsforAssign{
+ /* 
+   =Seats 
+   =Flights
+   =Reserving a seat
+    --> Is it availabe or empty
+   =Canceling a flight
+   --> print all the contact info for all passengers
+   =Canceling a reserved seat and making it available 
+   =Customers info
+   -->If its sold needs first and last name
+   -->address
+   -->phone number 
+   #Maybe First class and normal flight?
+ */
+ 
+  
+  
+  
+  
+}

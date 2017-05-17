66a099fac77885686152b6506a131e62e3bebb73
BluesLackSheep <438574207@qq.com>
2017-05-17 14:05:40 -0400

    Update objectsforAssign.java

--- objectsforAssign.java
+++ objectsforAssign.java
@@ -3,7 +3,9 @@ class objectsforAssign{
    =Seats 
    =Flights
    =Reserving a seat
-    --> Is it availabe or empty
+    --> Welcome customer to the airline
+    --> Tell customer type in 1 to book a fight, type in 2 to cancel a fight.(if type in anything except 1 or 2, return to welcome screen)
+        -->
    =Canceling a flight
    --> print all the contact info for all passengers
    =Canceling a reserved seat and making it available 
